0239640703c04b1702d8a8bce83586d3cd6adc8c
A. Shafie <litenova@outlook.com>
2024-07-25 11:55:03 +0200

    remove comments before json parse

--- src/Fuse.Cli/Minifiers/JsonMinifier.cs
+++ src/Fuse.Cli/Minifiers/JsonMinifier.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Fuse.Cli.Minifiers;
 
@@ -7,11 +8,19 @@ public static class JsonMinifier
 {
     public static string Minify(string content)
     {
-        using var doc = JsonDocument.Parse(content);
+        string jsonWithoutComments = RemoveComments(content);
+
+        using var doc = JsonDocument.Parse(jsonWithoutComments);
         using var stream = new MemoryStream();
         using var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = false });
         doc.WriteTo(writer);
         writer.Flush();
         return Encoding.UTF8.GetString(stream.ToArray());
+    }
+
+    private static string RemoveComments(string json)
+    {
+        var regex = new Regex(@"^\s*//.*$", RegexOptions.Multiline);
+        return regex.Replace(json, string.Empty);
     }
 }
